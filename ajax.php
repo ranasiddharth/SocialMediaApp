a0d60faf18135340e4ebbaaba1bc70450a8961e8
EC2 Default User <ec2-user@ip-172-31-26-111.us-east-2.compute.internal>
2021-05-16 12:40:52 +0000

    Add ajax functionality to check availability of username

---
+++ ajax.php
@@ -0,0 +1,23 @@
+<?php
+
+require('database.php');
+if(isset($_POST['username'])){
+$user = mysqli_real_escape_string($con, $_POST['username']);
+$query = "select count(*) as counter from siddharth_user where username='" . $user . "'";
+$result = mysqli_query($con, $query);
+$response = "<p style='color: green;'>Available.</p>";
+if(mysqli_num_rows($result)){
+    $row = mysqli_fetch_array($result);
+
+      $count = $row['counter'];
+    
+      if($count > 0){
+          $response = "<p style='color: red;'>Not Available.</p>";
+      }
+}
+echo $response;
+die;
+
+}
+
+?>
